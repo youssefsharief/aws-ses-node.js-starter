1ec81c05253f2ed1ab82a97236fa3c052985e88a
Youssef Sherif <sharief@aucegypt.edu>
2018-06-08 13:28:04 +0200

    Remove unneeded console log

--- src/core/ses.js
+++ src/core/ses.js
@@ -1,7 +1,6 @@
 const aws = require('aws-sdk');
 const config = require('../config/mailer.config')
 aws.config = {...aws.config, ...config}
-console.log('aaaaaaaaa')
 const ses = new aws.SES()
 
 module.exports = ses
